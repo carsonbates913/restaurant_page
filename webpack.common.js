800fb4b469169ce741373fc42c694b96aa382adc
Carson Bates <carson.d.bates.27@dartmouth.edu>
2024-07-30 16:47:19 -0400

    -feat: add image handling in webpack bundle

--- webpack.common.js
+++ webpack.common.js
@@ -16,12 +16,20 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  performance: {
+    maxAssetSize: 5000000,
+    maxEntrypointSize: 5000000,
+  },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
-}
+};
