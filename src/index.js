529e936381d1f9dc774c21350404f33f0305ce5f
Carson Bates <carson.d.bates.27@dartmouth.edu>
2024-07-30 16:47:42 -0400

    -feat: add changes to header an window structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import ('./home.css');
+import icon from './RestaurantFront.jpeg';
+
+const Icon = new Image();
+Icon.src = icon;
+
+
+const windowBackground = document.querySelector('.window-image');
+
+windowBackground.appendChild(Icon);
