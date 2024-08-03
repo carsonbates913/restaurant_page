c212075f70a8d7c18f353e7edccb1085fa69a2f9
Carson Bates <carson.d.bates.27@dartmouth.edu>
2024-08-03 16:46:27 -0400

    -feat: add menu item data

---
+++ src/components/menu.js
@@ -0,0 +1,122 @@
+export function createMenu (content) {
+  
+}
+
+const menuItemsData = [
+  {
+    title: 'Oysters on the Half Shell',
+    description: 'chef’s daily selection, hk mignonette, cocktail sauce',
+    price: '$28.00',
+  },
+  {
+    title: 'Steak Tartare',
+    description: 'sauce gribiche, crispy capers, cured egg yolk, brioche toast points',
+    price: '$34.00',
+  },
+  {
+    title: 'Shrimp Cocktail',
+    description: 'salsa coctel, avocado, plantain chips',
+    price: '$26.00',
+  },
+  {
+    title: 'Snapper Crudo',
+    description: 'pickled mango purèe, fresno chile, tiger’s milk, jalapeño oil, cilantro',
+    price: '$26.00',
+  },
+  {
+    title: 'Tuna Carpaccio',
+    description: 'ahi tuna, grain mustard vinaigrette, gooseberries, horseradish, pine nuts',
+    price: '$28,00',
+  },
+  {
+    title: 'Beef Wellington',
+    description: 'potato purée, glazed root vegetables, red wine demi-glace ',
+    price: '$74.00',
+  },
+  {
+    title: 'Chicken Scallopini',
+    description: 'chicken breast, roasted pee wee potatoes, artichokes, kale, truffle chicken',
+    price: '$36.00'
+  },
+  {
+    title: 'Crispy Skin Salmon',
+    description: 'coconut green curry, sticky rice, thai apple slaw',
+    price: '$44.00'
+  },
+  {
+    title: 'Grilled Branzino',
+    description: 'sauce vierge, swiss chard, almond gremolata, roasted cherry tomatoes',
+    price: '$60.00'
+  },
+  {
+    title: 'Braised Short Rib',
+    description: 'yukon potato cake, spinach, crispy onions, red wine demi-glace',
+    price: '$48.00'
+  },
+]
+
+const menuItemsAppetizers = [
+  {
+    title: 'Grilled Octopus',
+    description: 'white bean purée, pee wee potatoes, roasted tomatoes, chili oil, chimichurri',
+    price: '$32.00',
+  },
+  {
+    title: 'Wagyu Meatballs',
+    description: 'american wagyu, pork, slow-roasted tomato sauce, polenta croutons, parmesan, basil',
+    price: '$24.00',
+  },
+  {
+    title: 'Pan-Seared Scallops',
+    description: 'corn purée, braised bacon lardons, pickled shallots, chives',
+    price: '$32.00',
+  },
+  {
+    title: 'Lobster Risotto',
+    description: 'butter-poached lobster tail, braised fennel, confit lemon zest, parmesan',
+    price: '$38.00',
+  },
+  {
+    title: 'Sweet Corn Soup',
+    description: 'charred corn, jalapeño cornbread croutons, lime crema, chili oil',
+    price: '$16.00'
+  },
+  {
+    title: 'Tomato Burrata Salad',
+    description: 'heirloom tomatoes, white balsamic vinaigrette, basil crystals',
+    price: '$19.00'
+  },
+  {
+    title: 'Market Salad',
+    description: 'butter lettuce, avocado, radish, spiced cashews, pickled pineapple, green goddess',
+    price: '$ 15.00'
+  },
+  {
+    title: 'Caesar Salad',
+    description: 'parmesan frico, garlic croutons, lemon zest',
+    price: '$18.00'
+  },
+]
+
+const menuItemsDesserts = [
+  {
+    title: 'Vanilla Cheesecake',
+    description: 'graham cracker crust, lemon curd, blueberry compote',
+    price: '$15.00'
+  },
+  {
+    title: 'Coconut Three Ways',
+    description: 'mango passion fruit gelée, coconut sorbet, coconut cake croutons',
+    price: '$15.00'
+  },
+  {
+    title: 'Stawberry Shortcake Trifle',
+    description: 'sponge cake, pastry cream, hibiscus, macerated strawberries, toasted meringue, marcona almond crunch',
+    price: '$16.00'
+  },
+  {
+    title: 'Sticky Toffee Pudding',
+    description: 'english toffee sauce, dulce de leche ice cream',
+    price: '$17.00'
+  },
+]
